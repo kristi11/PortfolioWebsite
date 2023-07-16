7c543bfdde4cb9cab11f472e8591178f4ff6c741
kristi <tanellari@gmail.com>
2023-07-16 18:53:20 -0400

    wip

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -1,3 +1,4 @@
+@php use App\Models\Functions; @endphp
 <x-form-section submit="updateProfileInformation">
     <x-slot name="title">
         {{ __('Profile Information') }}
@@ -13,22 +14,23 @@
             <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
                 <!-- Profile Photo File Input -->
                 <input type="file" class="hidden"
-                            wire:model="photo"
-                            x-ref="photo"
-                            x-on:change="
+                       wire:model="photo"
+                       x-ref="photo"
+                       x-on:change="
                                     photoName = $refs.photo.files[0].name;
                                     const reader = new FileReader();
                                     reader.onload = (e) => {
                                         photoPreview = e.target.result;
                                     };
                                     reader.readAsDataURL($refs.photo.files[0]);
-                            " />
+                            "/>
 
-                <x-label for="photo" value="{{ __('Photo') }}" />
+                <x-label for="photo" value="{{ __('Photo') }}"/>
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}"
+                         class="rounded-full h-20 w-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
@@ -48,28 +50,31 @@
                     </x-secondary-button>
                 @endif
 
-                <x-input-error for="photo" class="mt-2" />
+                <x-input-error for="photo" class="mt-2"/>
             </div>
         @endif
 
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Name') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="state.name" autocomplete="name" />
-            <x-input-error for="name" class="mt-2" />
+            <x-label for="name" value="{{ __('Name') }}"/>
+            <x-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="state.name" autocomplete="name"/>
+            <x-input-error for="name" class="mt-2"/>
         </div>
 
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="email" value="{{ __('Email') }}" />
-            <x-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email" autocomplete="username" />
-            <x-input-error for="email" class="mt-2" />
+            <x-label for="email" value="{{ __('Email') }}"/>
+            <x-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email"
+                     autocomplete="username"/>
+            <x-input-error for="email" class="mt-2"/>
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) && ! $this->user->hasVerifiedEmail())
                 <p class="text-sm mt-2 dark:text-white">
                     {{ __('Your email address is unverified.') }}
 
-                    <button type="button" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" wire:click.prevent="sendEmailVerification">
+                    <button type="button"
+                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            wire:click.prevent="sendEmailVerification">
                         {{ __('Click here to re-send the verification email.') }}
                     </button>
                 </p>
@@ -82,26 +87,26 @@
             @endif
         </div>
 
-{{--        Job interest--}}
-            <div class="col-span-6 sm:col-span-4">
-                <x-label for="interest" value="{{ __('Interested in:') }}"/>
-                <select wire:model="state.interest"
-                        id="interest"
-                        name="interest"
-                        type="text"
-                        class="mt-1 block w-full focus:ring
+        {{--        Job interest--}}
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="interest" value="{{ __('Interested in:') }}"/>
+            <select wire:model.defer="state.interest"
+                    id="interest"
+                    name="interest"
+                    type="text"
+                    class="mt-1 block w-full focus:ring
                               focus:ring-opacity-50
                                border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm
                         ">
-                    class=""
-                    <option value="" class="text-gray-500" selected>Job types</option>
-                    @foreach(\App\Models\Functions::JOBS as $value => $name)
-                        <option value="{{ $value }}">{{ $name }}</option>
-                    @endforeach
+                class=""
+                <option value="" class="text-gray-500" selected>Job types</option>
+                @foreach(Functions::JOBS as $value => $name)
+                    <option value="{{ $value }}">{{ $name }}</option>
+                @endforeach
 
-                </select>
-                <x-input-error for="interest" class="mt-2"/>
-            </div>
+            </select>
+            <x-input-error for="interest" class="mt-2"/>
+        </div>
 
         <!-- Headline -->
         <div class="col-span-6 sm:col-span-4">
@@ -124,19 +129,19 @@
             <p class="text-gray-500 text-xs">Please enter the phone number in the following format 'xxx-xxx-xxxx' where
                 'x' represents the numbers</p>
             <x-input id="phone" type="tel" class="mt-1 block w-full" wire:model.defer="state.phone"
-                         placeholder="xxx-xxx-xxxx"/>
+                     placeholder="xxx-xxx-xxxx"/>
             <x-input-error for="phone" class="mt-2"/>
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <x-label for="city" value="{{ __('City') }}"/>
             <x-input
-                    id="city"
-                    name="city"
-                    type="text"
-                    wire:model.defer="state.city"
-                    placeholder="Enter city"
-                    class="mt-1 block w-full"
+                id="city"
+                name="city"
+                type="text"
+                wire:model.defer="state.city"
+                placeholder="Enter city"
+                class="mt-1 block w-full"
             />
             <x-input-error for="city" class="mt-2"/>
         </div>
@@ -152,32 +157,32 @@
                                ">
                 <option value="" class="text-gray-500" selected>Select state</option>
                 <optgroup label="North America">
-                    @foreach(\App\Models\Functions::NORTH_AMERICAN_STATES as $code => $name)
+                    @foreach(Functions::NORTH_AMERICAN_STATES as $code => $name)
                         <option value="{{ $code }}">{{ $name }}</option>
                     @endforeach
                 </optgroup>
                 <optgroup label="South America">
-                    @foreach(\App\Models\Functions::SOUTH_AMERICAN_STATES as $code => $name)
+                    @foreach(Functions::SOUTH_AMERICAN_STATES as $code => $name)
                         <option value="{{ $code }}">{{ $name }}</option>
                     @endforeach
                 </optgroup>
                 <optgroup label="Europe">
-                    @foreach(\App\Models\Functions::EUROPEAN_STATES as $code => $name)
+                    @foreach(Functions::EUROPEAN_STATES as $code => $name)
                         <option value="{{ $code }}">{{ $name }}</option>
                     @endforeach
                 </optgroup>
                 <optgroup label="Asia">
-                    @foreach(\App\Models\Functions::ASIAN_STATES as $code => $name)
+                    @foreach(Functions::ASIAN_STATES as $code => $name)
                         <option value="{{ $code }}">{{ $name }}</option>
                     @endforeach
                 </optgroup>
                 <optgroup label="Africa">
-                    @foreach(\App\Models\Functions::AFRICAN_STATES as $code => $name)
+                    @foreach(Functions::AFRICAN_STATES as $code => $name)
                         <option value="{{ $code }}">{{ $name }}</option>
                     @endforeach
                 </optgroup>
                 <optgroup label="Australia">
-                    @foreach(\App\Models\Functions::AUSTRALIAN_STATES as $code => $name)
+                    @foreach(Functions::AUSTRALIAN_STATES as $code => $name)
                         <option value="{{ $code }}">{{ $name }}</option>
                     @endforeach
                 </optgroup>
@@ -186,43 +191,43 @@
             <x-input-error for="state" class="mt-2"/>
         </div>
 
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-label for="country" value="{{ __('Country') }}"/>--}}
-{{--            <select wire:model="state.country"--}}
-{{--                    id="country"--}}
-{{--                    type="text"--}}
-{{--                    class="mt-1 block w-full border-gray-300--}}
-{{--                             focus:border-indigo-300 focus:ring--}}
-{{--                              focus:ring-indigo-200 focus:ring-opacity-50--}}
-{{--                               rounded-md shadow-sm">--}}
-{{--                <option value="" class="text-gray-500" selected>Select country</option>--}}
-{{--                    @foreach(\App\Models\Functions::COUNTRIES as $name => $code)--}}
-{{--                    <option value="{{ $name }}">{{ $name }}</option>--}}
-{{--                    @endforeach--}}
-{{--            </select>--}}
-{{--            <x-input-error for="country" class="mt-2"/>--}}
-{{--        </div>--}}
+        {{--        <div class="col-span-6 sm:col-span-4">--}}
+        {{--            <x-label for="country" value="{{ __('Country') }}"/>--}}
+        {{--            <select wire:model="state.country"--}}
+        {{--                    id="country"--}}
+        {{--                    type="text"--}}
+        {{--                    class="mt-1 block w-full border-gray-300--}}
+        {{--                             focus:border-indigo-300 focus:ring--}}
+        {{--                              focus:ring-indigo-200 focus:ring-opacity-50--}}
+        {{--                               rounded-md shadow-sm">--}}
+        {{--                <option value="" class="text-gray-500" selected>Select country</option>--}}
+        {{--                    @foreach(\App\Models\Functions::COUNTRIES as $name => $code)--}}
+        {{--                    <option value="{{ $name }}">{{ $name }}</option>--}}
+        {{--                    @endforeach--}}
+        {{--            </select>--}}
+        {{--            <x-input-error for="country" class="mt-2"/>--}}
+        {{--        </div>--}}
 
-{{--        Linkedin--}}
+        {{--        Linkedin--}}
         <div class="col-span-6 sm:col-span-4">
             <x-label for="linkedin_link" value="{{ __('Linkedin link') }}"/>
             <x-input id="linkedin_link"
-                         type="text"
-                         class="mt-1 block w-full"
-                         wire:model.defer="state.linkedin_link"
-                         placeholder="Add your Linkedin account link"
+                     type="text"
+                     class="mt-1 block w-full"
+                     wire:model.defer="state.linkedin_link"
+                     placeholder="Add your Linkedin account link"
             />
             <x-input-error for="linkedin_link" class="mt-2"/>
         </div>
 
-{{--        Github--}}
+        {{--        Github--}}
         <div class="col-span-6 sm:col-span-4">
             <x-label for="github_link" value="{{ __('Github link') }}"/>
             <x-input id="github_link"
-                         type="text"
-                         class="mt-1 block w-full"
-                         wire:model.defer="state.github_link"
-                         placeholder="Add your Github account link"
+                     type="text"
+                     class="mt-1 block w-full"
+                     wire:model.defer="state.github_link"
+                     placeholder="Add your Github account link"
             />
             <x-input-error for="github_link" class="mt-2"/>
         </div>
