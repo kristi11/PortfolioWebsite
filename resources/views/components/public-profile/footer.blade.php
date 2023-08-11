4ee4a086df77871f077c2f96d9d5bd1648e30350
kristi <tanellari@gmail.com>
2023-08-11 13:36:34 -0400

    Fix footer bug

--- resources/views/components/public-profile/footer.blade.php
+++ resources/views/components/public-profile/footer.blade.php
@@ -2,7 +2,7 @@
 <x-public-profile.footer-s-v-g/>
 
 <section class="container mx-auto text-center py-6 mb-12">
-    <div class="grid lg:grid-cols-2">
+    <div class="grid lg:grid-cols-2 gap-y-10">
         <div class="grid-cols-1">
             <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
                 Have a question?
@@ -10,7 +10,7 @@
             <div class="w-full mb-4">
                 <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
             </div>
-            <h3 class="my-4 text-3xl leading-tight">
+            <h3 class="my-8 text-3xl leading-tight">
                 Feel free to contact me.
             </h3>
             <livewire:secure-mailto email="{{$user->email}}"/>
